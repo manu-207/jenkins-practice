847ef069a13592a101176a13dd1e574d8acd882a
manu-207 <lovelymanu207@gmail.com>
2023-09-07 12:10:30 +0530

    gd

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
@@ -10,5 +10,4 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  version = "3.46.0"
 }
